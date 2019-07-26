6a3a75b2f576c7183cdbe9feed433673b913cf33
ecn3 <ecn3@students.uwf.edu>
2019-07-26 11:45:43 -0500

    Added example code for nano pwm. Still needs to be tested.

--- SensorsTests/PWM/PWMEx.py
+++ SensorsTests/PWM/PWMEx.py
@@ -1,14 +1,14 @@
-import RPi.GPIO as IO          #calling header file which helps us use GPIO’s of PI
+import RPi.GPIO as IO          #calling header file which helps us use GPIOs of PI
 
 import time                            #calling time to provide delays in program
 
 IO.setwarnings(False)           #do not show any warnings
 
-IO.setmode (IO.BCM)         #we are programming the GPIO by BCM pin numbers. (PIN35 as ‘GPIO19’)
+IO.setmode (IO.BOARD)         #we are programming the GPIO by BCM pin numbers. (PIN35 as GPIO19)
 
-IO.setup(18,IO.OUT)           # initialize GPIO19 as an output.
+IO.setup(22,IO.OUT)           # initialize GPIO19 as an output.
 
-p = IO.PWM(18,100)                   #GPIO19 as PWM output, with 100Hz frequency
+p = IO.PWM(22,100)                   #GPIO19 as PWM output, with 100Hz frequency
 p.start(0)                          #generate PWM signal with 0% duty cycle
 
 while 1:                               #execute loop forever
---
+++ SensorsTests/PWM/PWMEx_Nano.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append('/opt/nvidia/jetson-gpio/lib/python')
+
+import RPi.GPIO as GPIO          #calling header file which helps us use GPIOs of PI
+import time                            #calling time to provide delays in program
+'''
+output_pins = {
+    'JETSON_XAVIER': 18,
+    'JETSON_NANO': 33,
+}
+'''
+output_pin = 33
+if output_pin is None:
+    raise Exception('PWM not supported on this board')
+
+def main():
+    # Pin Setup:
+    # Board pin-numbering scheme
+    GPIO.setmode(GPIO.BOARD)
+    # set pin as an output pin with optional initial state of HIGH
+    GPIO.setup(output_pin, GPIO.OUT, initial=GPIO.HIGH)
+    p = GPIO.PWM(output_pin, 50)
+    p.start(25)
+
+    print("PWM running. Press CTRL+C to exit.")
+    try:
+        while True:
+            time.sleep(1)
+    finally:
+        p.stop()
+        GPIO.cleanup()
+
+if __name__ == '__main__':
+    main()
