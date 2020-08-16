bffbff10954b77df4ab9fae8858283d78ce007ab
brendonortolano <bmo6@students.uwf.edu>
2020-08-16 15:24:46 -0500

    Fixed nonlocal var error and added support for other controllers.

--- Python/Controls/Cart.py
+++ Python/Controls/Cart.py
@@ -7,16 +7,21 @@ import Controller
 import time
 
 #CONTROLLER FLAGS
-control_type = 'LQR'
+#control_type = 'LQR'
 #control_type = 'PID'
 #control_type = 'PD'
-#control_type = 'COMBINED_PID'
+control_type = 'COMBINED_PID'
 
 #Controller Vectors
-k = [-10.0000/1.5, -29.9836/1.5, 822.2578, 85.5362]
-pd = [0, 0, 0]
-pid = [0, 0, 0]
-comb_pid = [[0, 0, 0], [0, 0, 0]]
+k = [-10.0000/1.5, -30/1.5, 822, 85.5]
+pd = [3990, 0, 171]
+pid = [1431, 3768, 135]
+
+comb_pid = [[1907, 4233, 201], [1936, 406, -15]] #Theta PID vector, x PID vector
+theta_weight = 0.8
+x_weight = 0.2
+
+comb_pid = [[elem * theta_weight for elem in comb_pid[0]], [elem * x_weight for elem in comb_pid[1]]]
 
 #Set Points
 SET_PT_THETA = -3.75 * 3.141592 / 180
@@ -24,25 +29,29 @@ SET_PT_X = 0
 
 #Global Variables
 max_pwm = 85
-frequency = 50 #PWM Frequency in Hz
 max_theta = 15
+max_x = 1
+
+frequency = 50 #PWM Frequency in Hz
 pwm_offset = 16.67
-arduino_port = '/dev/ttyUSB1'
+
+arduino_port = '/dev/ttyUSB0'
 
 class Cart():
     
     def __init__(self):
-        global k, pd, pid, comb_pid, max_pwm, frequency, max_theta, pwm_offset, arduino_port
+        global k, pd, pid, comb_pid, max_pwm, max_theta, max_x, frequency, pwm_offset, arduino_port
         #Cart variables
         self.max_pwm = max_pwm
-        self.frequency = frequency
         self.max_theta = max_theta
+        self.max_x = max_x
+        self.frequency = frequency
         self.pwm_offset = pwm_offset
         self.arduino_port = arduino_port
 
-        #Motor, Controller, and Camera Instance Creation
+        #Motor, Controller, and Camera Instance Definition
         self.motors = Motors.Motors(max_pwm = self.max_pwm, frequency=self.frequency, arduino_port=self.arduino_port)
-        self.controller = Controller.Controller(max_theta = self.max_theta)
+        self.controller = Controller.Controller(max_theta = self.max_theta, max_x = self.max_x)
         self.camera = Camera.Camera()
     
         #Controller Vectors
@@ -64,14 +73,13 @@ class Cart():
 
             if self.status:
                 if control_type == 'LQR':
-                    self.status, self.duty_cycle = self.controller.LQR(self.angle, self.pos, K=self.k, set_pt_theta = SET_PT_THETA, set_pt_x = SET_PT_X, stat = 1)
+                    self.status, self.duty_cycle = self.controller.LQR(self.angle, self.pos, K=self.k, set_pt_theta = SET_PT_THETA, set_pt_x = SET_PT_X)
                 elif control_type == 'PID':
-                    self.status, self.duty_cycle = self.controller.PID(self.angle, pid=self.pid, set_pt_theta = SET_PT_THETA, stat = 1)
+                    self.status, self.duty_cycle = self.controller.PID(self.angle, pid=self.pid, set_pt_theta = SET_PT_THETA)
                 elif control_type == 'PD':
-                    self.status, self.duty_cycle = self.controller.PID(self.angle, pid=self.pd, set_pt_theta = SET_PT_THETA, stat = 1)
+                    self.status, self.duty_cycle = self.controller.PID(self.angle, pid=self.pd, set_pt_theta = SET_PT_THETA)
                 elif control_type == 'COMBINED_PID':
-                    #self.status, self.duty_cycle = self.controller.PID(self.angle, self.pos, comb_pid=self.comb_pid, set_pt_theta = SET_PT_THETA, set_pt_x = SET_PT_X, stat = 1)
-                    pass
+                    self.status, self.duty_cycle = self.controller.comb_PID(self.angle, self.pos, comb_pid=self.comb_pid, set_pt_theta = SET_PT_THETA, set_pt_x = SET_PT_X)
             else:
                 self.controller.PID(0)
                 sleep_t = int(round(time.time() * 1000))
@@ -79,6 +87,7 @@ class Cart():
                     self.angle = self.camera.get_angle()
                     break_flag = self.camera.check_for_break()
                 self.status = 1
+                self.controller.reset()
                 continue
             
             if self.duty_cycle > 0:
@@ -91,6 +100,6 @@ class Cart():
         self.camera.close()
 
 if __name__=="__main__":
-    Cart = Cart()
-    Cart.run()
+    cart = Cart()
+    cart.run()
         
--- Python/Controls/Controller.py
+++ Python/Controls/Controller.py
@@ -2,18 +2,22 @@
 import time
 
 class Controller():
-    def __init__(self, max_theta):
-        self.curr_time = self.prev_time = 0
+    def __init__(self, max_theta=15, max_x=1):
+        self.curr_time = self.prev_time = int(round(time.time() * 1000))
         self.prev_x = self.prev_theta = 0
-        self.max_theta = 15
+        self.max_theta = max_theta
+        self.max_x = max_x
         self.theta_integral = 0
+        self.x_integral = 0
 
     def derivative(self, new, last, thisTime, lastTime): #Find the derivative of theta
         dt = thisTime - lastTime
-        derive = (new - last)/(float(dt)/1000)
-        return derive
+        if dt != 0:
+            derive = (new - last)/(float(dt)/1000)
+            return derive
+        else: return 0
 
-    def LQR(self, theta, x, K=[0,0,0,0], set_pt_theta = 0, set_pt_x = 0, stat = 1, offset=0):
+    def LQR(self, theta, x, K=[0,0,0,0], set_pt_theta = 0, set_pt_x = 0):
         if (theta > self.max_theta or theta < -self.max_theta):
             return 0,0
         theta *= 3.1415926/180
@@ -36,9 +40,9 @@ class Controller():
         print("DC: ", duty_cycle)
         print("DELTA: ", self.curr_time - pt)
 
-        return stat, duty_cycle
+        return 1, duty_cycle
 
-    def PID(self, theta, pid=[0,0,0], set_pt_theta = 0, stat = 1, offset=0):
+    def PID(self, theta, pid=[0,0,0], set_pt_theta = 0, stat = 1):
         [Kp, Ki, Kd] = pid
         
         if (theta > self.max_theta or theta < -self.max_theta):
@@ -47,7 +51,7 @@ class Controller():
         
         self.curr_time = int(round(time.time() * 1000))  # get the current time
         theta_dot = self.derivative(theta, self.prev_theta, self.curr_time, self.prev_time)
-        self.theta_integral += theta * (self.curr_time - self.prev_time) 
+        self.theta_integral += theta * (self.curr_time - self.prev_time) / 1000
        
         duty_cycle = Kp * (theta - set_pt_theta) + Ki * self.theta_integral + Kd * theta_dot
 
@@ -59,4 +63,41 @@ class Controller():
         print("DC: ", duty_cycle)
         print("DELTA: ", self.curr_time - pt)
 
-        return stat, duty_cycle
+        return 1, duty_cycle
+
+    def PID_pos(self, x, pid=[0,0,0], set_pt_x = 0):
+        [Kp, Ki, Kd] = pid
+
+        if (x > self.max_x or x < -self.max_x):
+            return 0,0
+        
+        self.curr_time = int(round(time.time() * 1000))  # get the current time
+        x_dot = self.derivative(x, self.prev_x, self.curr_time, self.prev_time)
+        self.x_integral += x * (self.curr_time - self.prev_time) / 1000
+       
+        duty_cycle = Kp * (x - set_pt_x) + Ki * self.x_integral + Kd * x_dot
+
+        pt = self.prev_time
+        self.prev_time = self.curr_time
+        self.prev_x = x
+        
+        print("Time:", self.curr_time, "\n")
+        print("DC: ", duty_cycle)
+        print("DELTA: ", self.curr_time - pt)
+
+        return 1, duty_cycle
+
+    def comb_PID(self, theta, x, comb_pid = [[0,0,0],[0,0,0]], set_pt_theta = 0, set_pt_x = 0):
+        theta_stat, theta_dc = self.PID(theta, comb_pid[0], set_pt_theta)
+        x_stat, x_dc = self.PID_pos(x, comb_pid[1], set_pt_x)
+
+        return min(theta_stat, x_stat), theta_dc + x_dc
+
+    def reset(self):
+        self.curr_time = self.prev_time = int(round(time.time() * 1000))
+        self.theta_integral = 0
+        self.x_integral = 0
+
+    def toFile(self):
+        #TODO
+        pass
